7864269c2dda24daee99bc5bae3850e89534217d
Murillo_Lopes <murillolopes9335@gmail.com>
2025-05-01 11:50:01 -0300

    Aulas

---
+++ aula_37_statelesswidget/android/app/src/main/kotlin/com/example/aula_37_statelesswidget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_37_statelesswidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ aula_38e39_statefulwidget/android/app/src/main/kotlin/com/example/aula_38e39_statefulwidget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_38e39_statefulwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
