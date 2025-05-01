0e7d9603d275a1f6f0e71f29de03c91fb4cf0d03
Murillo_Lopes <murillolopes9335@gmail.com>
2025-05-01 12:22:39 -0300

    Aulas

---
+++ aula_40e41_private_members/android/app/src/main/kotlin/com/example/aula_40e41_private_members/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_40e41_private_members
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ aula_42_creating_question_widget/android/app/src/main/kotlin/com/example/aula_42_creating_question_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aula_42_creating_question_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
